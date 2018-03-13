0398ea1baad2266712220e79dcf2cb2ebae704ec
Eugene <yauheni_zinchanka@epam.com>
2018-03-14 00:51:28 +0300

    Implemented function longestConsecutiveLength, done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+
+  if (array.length == 0) {
+    return 0
+  }
+  
+  const defaultValue = 1
+  var result = defaultValue
+  var counter = defaultValue
+  var nextValue = -1
+
+  function compare(a, b) {
+    return a-b
+  }
+  array.sort(compare)
+
+  for (let index = 0; index < array.length; index++) {
+    var value = array[index];
+
+    if (index >= 1) {
+      if (array[index-1] == value) {
+        nextValue = ++value;
+        continue;
+      }
+    }
+
+    if (value == nextValue) {
+        ++counter;
+    } else {
+       if (counter > result) {
+         result = counter
+       }
+       counter = defaultValue
+    }
+
+    nextValue = ++value;
+  }
+
+  if (counter > result) {
+    result = counter
+  }
+
+  return result
 }
